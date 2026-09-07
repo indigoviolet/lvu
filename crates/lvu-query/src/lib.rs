ade23f761ee1cc92d300781c5557de98f77df056
Venky Iyer <indigoviolet@gmail.com>
2026-09-07 02:44:01 +0000

    Revert "Recognize event times natively and declare arbitrary time bases"
    This reverts commit 5a376710fe9d215885c871540a532f91a7f4e12f.

--- crates/lvu-query/src/lib.rs
+++ crates/lvu-query/src/lib.rs
@@ -9,7 +9,6 @@ pub mod host;
 pub mod parquet;
 pub mod regex_enrichment;
 pub mod state;
-pub mod time_field;
 pub mod validate;
 
 pub use adapter::*;
@@ -18,5 +17,4 @@ pub use host::*;
 pub use parquet::*;
 pub use regex_enrichment::*;
 pub use state::*;
-pub use time_field::*;
 pub use validate::*;
